ac0387cf8de6c79bb98be8f13a51a8d49f48a22f
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-06-08 07:58:29 -0400

    Brands location change

--- src/data/featuredBrands.js
+++ src/data/featuredBrands.js
@@ -1,8 +1,8 @@
 const brands = [
-  { name: "Emtek", src: "/src/assets/logos/emtek.png" },
-  { name: "Taymor", src: "/src/assets/logos/taymor.png" },
-  { name: "Weiser", src: "/src/assets/logos/weiser.png" },
-  { name: "Richelieu", src: "/src/assets/logos/richelieu.png" },
+  { name: "Emtek", src: "/logos/emtek.png" },
+  { name: "Taymor", src: "/logos/taymor.png" },
+  { name: "Weiser", src: "/logos/weiser.png" },
+  { name: "Richelieu", src: "/logos/richelieu.png" },
 ];
 
 export default brands;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import React from "react";
 
 
 const Home = () => {
-  return (
+  return ( 
     <>
       <Hero />
       <FeaturedCategories />
